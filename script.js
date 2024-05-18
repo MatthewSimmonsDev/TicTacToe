3460569e8492160ea93e2f696fcf3e808f1c1b85
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-05-18 15:20:53 -0400

    Update script.js
    Cleaned up for next big step.

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
+
+// Main Game Logic Section
+// Create game board
 const gameBoard = (function () {
     const createGameBoard = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
     return createGameBoard;
 })()
 
-
+// Create player factory
 const player = (function (player, piece) {
     return {player, piece};
 })
 
+// Create players one and two
 const createPlayers = (function () {
     const playerOne = player("Player One", "X");
-    // playerOne.piece = "X";
     const playerTwo = player("Player Two", "O");
-    // playerTwo.piece = "O";
     return {playerOne, playerTwo};
 })()
 
+// Game logic
 const gameController = (function () {
     let turn = 1;
     
 })()
 
+// Run as console game
 const runConsoleGame = (function () {
 
     console.log(gameBoard)
 })()
+
 
 function incrementTurn(turn){
     turn++;
