90d5b60a338cffcf2c9fc6c9a3628c3550054308
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-05-19 16:16:39 -0400

    Update script.js
    fiddling with stuff not working

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 
 const globalVariables = {
     turn : 1,
-    
+    prompt : "",
+    piece : ""
 }
+
 // Create game board
 const gameBoard = (function () {
     const createGameBoard = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]];
@@ -25,52 +27,91 @@ const createPlayers = (function () {
 
 // Game logic
 const gameController = (function () {
-    displayGameBoard();
-    instructPlayerToPlacePiece();
+    console.log(gameBoard)
+    while (globalVariables.turn < 10){
+        instructPlayerToPlacePiece();
+        
+    }
+    console.log(gameBoard)
     
 })()
 
 function instructPlayerToPlacePiece(){
-    let prompt;
     if (globalVariables.turn % 2 === 1){
-        prompt = window.prompt("X piece select an available location.");
+        globalVariables.prompt = window.prompt("X piece select an available location.");
         globalVariables.turn ++;
-        
-        console.log(globalVariables.turn);
-        
+        globalVariables.piece = "X"
+        1
     }
-    else if (globalVariables.turn % 2 !== 1){11
-        prompt = window.prompt("O piece select an available location.");
+    else if (globalVariables.turn % 2 !== 1){
+        globalVariables.prompt = window.prompt("O piece select an available location.");
         globalVariables.turn++
-        
-        console.log(globalVariables.turn);
+        globalVariables.piece = "O"
         
     }
     
-    displayGameBoard();
-    checkTurns();
 }
 
 function displayGameBoard(){
-    
+    let arrIncrementer = 1;
     for (let i = 0; i <= 2; i++){
         for (let j = 0; j <= 2; j++){
-            gameBoard[i][j] = " . "
+            
+            if([i][j] != " X " && [i][j] != " O "){
+                
+                if (i == 0){
+                    gameBoard[i][j] = ` ${arrIncrementer} `
+                    arrIncrementer++;
+                }
+                if (i == 1){
+                    gameBoard[i][j] = ` ${arrIncrementer} `
+                    arrIncrementer++;
+                }
+                if (i == 2){
+                    gameBoard[i][j] = ` ${arrIncrementer} `
+                    arrIncrementer++;
+                }
+            }
+            
         }
     }
+    console.log(gameBoard);
     
-    console.log(gameBoard)
 }
 
-function checkTurns(){
-    if( globalVariables.turn <= 9){
-        instructPlayerToPlacePiece();
-    } else {
-        checkForEndgame();
+function changePromptToPiece(prompt){
+    if (prompt === "1"){
+        gameBoard[0][0] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "2"){
+        gameBoard[0][1] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "3"){
+        gameBoard[0][2] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "4"){
+        gameBoard[1][0] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "5"){
+        gameBoard[1][1] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "6"){
+        gameBoard[1][2] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "7"){
+        gameBoard[2][0] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "8"){
+        gameBoard[2][1] = ` ${globalVariables.piece} `
+    }else
+    if (prompt === "9"){
+        gameBoard[2][2] = ` ${globalVariables.piece} `
     }
+
 }
+
 function checkForEndgame(){
-    if (globalVariables.turn >= 9) {
+    if (globalVariables.turn > 9) {
         //trigger game over
         console.log("Game Over");
         
